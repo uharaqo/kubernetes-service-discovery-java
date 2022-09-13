d4b1afebcfd0e6552973f7643719154252c4375a
uharaqo <uharaqo@gmail.com>
2022-09-12 19:45:14 -0700

    setting up gradle, github actions, publish (1)

---
+++ src/test/java/com/github/uharaqo/k8s/discovery/MainTest.java
@@ -0,0 +1,13 @@
+package com.github.uharaqo.k8s.discovery;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  void dummy() {
+    assertTrue(true);
+  }
+}
